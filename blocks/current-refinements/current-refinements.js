2aaf9bee5a0ba35a7e568472629508c1c4ea121d
Nithin Asokan <2903839+nithinasokan@users.noreply.github.com>
2023-09-06 12:39:14 -0700

    ASSETS-28417: Styling for current refinements (#83)
    * ASSETS-28417: Styling for current refinements
    
    * ASSETS-28417: Update styling
    
    * ASSETS-28417: Update styling for refinements
    
    * ASSETS-28417: Use styles for font and colors
    
    * ASSETS-28417: Add more padding to align with design
    
    * ASSETS-28417: Fix styling when single asset is chosen
    
    * ASSETS-28417: Styling for asset quick view
    
    * ASSETS-28417: Match current refinement & metadata styles

--- blocks/current-refinements/current-refinements.js
+++ blocks/current-refinements/current-refinements.js
@@ -18,34 +18,65 @@ export default function decorate(block) {
   currentRefinements.classList.add('current-refinements');
   block.appendChild(currentRefinements);
 
-  window.search.addWidgets([
-    instantsearch.widgets.currentRefinements(
-      {
-        container: '#current-refinements',
-        transformItems: (items) => {
-          for (let i = 0; i < items.length; i += 1) {
-            // eslint-disable-next-line max-len
-            items[i].label = refinementsByProperty[items[i].attribute] ? refinementsByProperty[items[i].attribute].name : items[i].label;
-            items[i].refinements.forEach((refinement) => {
-              const refinementConfig = refinementsByProperty[refinement.attribute];
-              if (refinementConfig && refinementConfig['data-type']) {
-                refinement.label = formatAssetMetadata(refinementConfig.attribute, refinement.label, refinementConfig['data-type']);
-              }
-            });
-          }
-          return items;
-        },
-      },
-    ),
-  ]);
+  const renderCurrentRefinements = (renderOptions, isFirstRender) => {
+    const {
+      items, widgetParams, refine,
+    } = renderOptions;
 
-  const obs = new MutationObserver(() => {
-    const currentRefinementItems = block.querySelectorAll('.ais-CurrentRefinements-delete');
-    currentRefinementItems.forEach((currentRefinementItem) => {
-      currentRefinementItem.addEventListener('click', () => {
-        closeAssetDetails();
+    if (isFirstRender) {
+      const currRefinementsDivEl = document.createElement('div');
+      currRefinementsDivEl.classList.add('ais-CurrentRefinements');
+
+      const currrefinementListEl = document.createElement('ul');
+      currrefinementListEl.classList.add('ais-CurrentRefinements-list', 'refinement-list');
+
+      currRefinementsDivEl.appendChild(currrefinementListEl);
+      widgetParams.container.appendChild(currRefinementsDivEl);
+      return;
+    }
+
+    const refinementsEl = widgetParams.container.querySelector('.ais-CurrentRefinements-list');
+    refinementsEl.innerHTML = '';
+
+    const createRefinementDataAttribtues = (refinement) => Object.keys(refinement)
+      .map((key) => `data-${key}="${refinement[key]}"`)
+      .join(' ');
+    items.forEach((item) => {
+      item.refinements.forEach((refinement) => {
+        // decorate facet type
+        const facetType = refinementsByProperty[refinement.attribute]
+          ? refinementsByProperty[refinement.attribute].name : refinement.label;
+        // decorate facet value
+        let facetValue = refinement.value;
+        const refinementConfig = refinementsByProperty[refinement.attribute];
+        if (refinementConfig && refinementConfig['data-type']) {
+          facetValue = formatAssetMetadata(refinementConfig.attribute, refinement.label, refinementConfig['data-type']);
+        }
+        const li = document.createElement('li');
+        li.classList.add('ais-CurrentRefinements-item', 'refinement-item');
+        li.innerHTML = `
+          <span class="ais-CurrentRefinements-label refinement-label">${facetType} -</span>
+          <span class="ais-CurrentRefinements-category refinement-category">
+            <span class="ais-CurrentRefinements-categoryLabel refinement-category-label">${facetValue}</span>
+            <button class="ais-CurrentRefinements-delete refinement-delete" ${createRefinementDataAttribtues(refinement)}></button>
+          </span>
+        `;
+        li.querySelector('button').addEventListener('click', () => {
+          refine(refinement);
+          closeAssetDetails();
+        });
+        refinementsEl.appendChild(li);
       });
     });
-  });
-  obs.observe(block, { childList: true, subtree: true });
+  };
+
+  const customCurrentRefinements = instantsearch.connectors.connectCurrentRefinements(
+    renderCurrentRefinements,
+  );
+
+  window.search.addWidgets([
+    customCurrentRefinements({
+      container: document.querySelector('#current-refinements'),
+    }),
+  ]);
 }
