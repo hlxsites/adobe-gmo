11fd6ae4dce9ed486eec5db37be8afd78e7d0790
Ankith Aiyar <aiyarankith@users.noreply.github.com>
2023-10-19 15:16:29 -0700

    ASSETS-30432 - Collection API request & response (#135)
    * ASSETS-30432: Collections API requests and response
    
    * ASSETS-30432: Collections API requests and response
    
    * Same headers set
    
    * address pr comments and add block for collections page
    
    * Adding sample code to readme
    
    * throw error on failed to delete
    
    * Update scripts/collections.js
    
    * Update collections.js
    
    * Update scripts/collections.js
    
    ---------
    
    Co-authored-by: aaiyar <aaiyar@adobe.com>
    Co-authored-by: Andrew Khoury <andrewmkhoury@gmail.com>

---
+++ blocks/collections/collections.js
@@ -0,0 +1,6 @@
+import {
+  getCollection, listCollection, createCollection, deleteCollection, patchCollection,
+} from '../../scripts/collections.js';
+
+export default async function decorate(block) {
+}
