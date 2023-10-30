00f3ce315df6b9eb9d3a5e7e3a363e7069ce4085
Tyrone Tse <ttse@adobe.com>
2023-10-30 16:15:02 -0500

    Removed the 3 Events for now PREVIOUS_ASSET: 'previous-asset' NEXT_ASSET: 'next-asset' SEARCH_RESULTS_CHANGED: 'search-results-changed'

--- scripts/adobe-data-layer.js
+++ scripts/adobe-data-layer.js
@@ -19,9 +19,6 @@ export function loadDataLayer() {
   document.addEventListener(EventNames.ASSET_SELECTED, (e) => addDataLayer(e,e.detail));
   document.addEventListener(EventNames.ASSET_DESELECTED, (e) => addDataLayer(e,e.detail));
   document.addEventListener(EventNames.ADD_ITEM_MULTISELECT, (e) => addDataLayer(e,e.detail));
-  document.addEventListener(EventNames.PREVIOUS_ASSET, (e) => addDataLayer(e,e.detail));
-  document.addEventListener(EventNames.NEXT_ASSET, (e) => addDataLayer(e,e.detail));
-  document.addEventListener(EventNames.SEARCH_RESULTS_CHANGED, (e) => addDataLayer(e,e.detail));
   document.addEventListener(EventNames.ASSET_QUICK_PREVIEW_CLOSE, (e) => addDataLayer(e,e.detail));
   document.addEventListener(EventNames.CLOSE_BANNER, (e) => addDataLayer(e,e.detail));
 }
