b9a2f71d2328085d015bdba867fdbe14b5645047
Andrew Khoury <andrewmkhoury@gmail.com>
2023-11-14 14:22:31 -0800

    ASSETS-31302 - Add delete collection button to /collection/... page (#216)

--- blocks/adp-collection-header/adp-collection-header.js
+++ blocks/adp-collection-header/adp-collection-header.js
@@ -1,20 +1,46 @@
-import { getCollection, getCollectionIdFromURL } from '../../scripts/collections.js';
+import { getCollection, getCollectionIdFromURL, deleteCollection } from '../../scripts/collections.js';
+import createConfirmDialog from '../../scripts/confirm-dialog.js';
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 
 function createCollectionInfoHeader(collectionInfoHeader, collection) {
   // include back to collections listing similar to hide filters button
   collectionInfoHeader.innerHTML = `
-        <div class="back-button">
-            <a href="/collections"><span class="icon icon-filter-open"></span><span class="text">Back to Collections</span></a>
+        <div class="adp-collection-header-left">
+          <div class="back-button">
+              <a href="/collections"><span class="icon icon-back"></span></a>
+          </div>
+          <div class="adp-collection-header-collection-info">
+            <div class="adp-collection-title"></div>
+            <div class="adp-collection-subinfo">
+              <div class="adp-collection-stats"></div>
+            </div>
+          </div>
         </div>
-        <div class="collection-title">
-        ${collection.title}
-        </div>
-        <div class="stats">
-            (<span class="stats-count-label">${collection.items.length}</span><span class="stats-text-label"> assets</span>)
+        <div class="adp-collection-header-right actions">
+          <button class="adp-action adp-danger action-collection-delete">
+            <span class="icon icon-delete"></span>Delete Collection
+          </a>
         </div>
     `;
+  collectionInfoHeader.querySelector('.adp-collection-title').innerText = collection.title;
+  collectionInfoHeader.querySelector('.adp-collection-stats').innerText = `${collection.items.length} items`;
+  collectionInfoHeader.querySelector('.action-collection-delete').addEventListener('click', async (e) => {
+    e.preventDefault();
+    const collectionId = getCollectionIdFromURL();
+    if (collectionId) {
+      createConfirmDialog(
+        'Delete collection',
+        `Are you sure you want to delete the collection "${collection.title}"?`,
+        async () => {
+          await deleteCollection(collectionId);
+          window.location.href = '/collections';
+        },
+        'Proceed',
+      );
+    }
+  });
   decorateIcons(collectionInfoHeader);
+  return collectionInfoHeader;
 }
 
 export default async function decorate(block) {
@@ -23,7 +49,6 @@ export default async function decorate(block) {
   const collectionId = getCollectionIdFromURL();
   if (collectionId) {
     const collection = await getCollection(collectionId);
-    const collectionInfoHeader = createCollectionInfoHeader(block, collection);
-    block.appendChild(collectionInfoHeader);
+    createCollectionInfoHeader(block, collection);
   }
 }
--- scripts/confirm-dialog.js
+++ scripts/confirm-dialog.js
@@ -1,12 +1,12 @@
 import { decorateIcons } from './lib-franklin.js';
 
-export default async function createConfirmDialog(title, text, onCancel, onConfirm, cancelButtonText, confirmButtonText) {
+export default async function createConfirmDialog(title, text, onConfirm, confirmButtonText, onCancel, cancelButtonText) {
   const dialogContainer = document.createElement('div');
   dialogContainer.classList.add('confirm-dialog-wrapper');
   dialogContainer.innerHTML = `<dialog autofocus>
     <div class='dialog-container'>
       <div class='dialog-header'>
-        <div class='dialog-header-left'>${title}</div>
+        <div class='dialog-header-left'></div>
         <div class='dialog-header-right'>
           <button class='action action-close' aria-label='Close'>
             <span class='icon icon-close'></span>
@@ -14,30 +14,48 @@ export default async function createConfirmDialog(title, text, onCancel, onConfi
         </div>
       </div>
       <div class='dialog-body'>
-        <div class='dialog-body-content'>${text}</div>
+        <div class='dialog-body-content'></div>
       </div>
       <div class='dialog-footer'>
         <div class='actions-container'>
-          <button class='confirm-dialog-button cancel'>${cancelButtonText}</button>
-          <button class='confirm-dialog-button confirm'>${confirmButtonText}</button>
+        <button class="adp-action adp-cancel action-cancel"></button>
+        <button class="adp-action adp-danger action-confirm"></button>
         </div>
       </div>
     </div>
   </dialog>`;
+  addContent(dialogContainer.querySelector('.dialog-header-left'), title);
+  addContent(dialogContainer.querySelector('.dialog-body-content'), text);
+  addContent(dialogContainer.querySelector('.action-cancel'), cancelButtonText || 'Cancel');
+  addContent(dialogContainer.querySelector('.action-confirm'), confirmButtonText || 'Confirm');
 
-  dialogContainer.querySelector('.cancel').addEventListener('click', () => {
-    onCancel();
-    dialogContainer.remove();
+  dialogContainer.querySelector('.action-cancel').addEventListener('click', () => {
+    if (onCancel) onCancel();
+    closeConfirmationDialog(dialogContainer);
   });
-  dialogContainer.querySelector('.confirm').addEventListener('click', () => {
+  dialogContainer.querySelector('.action-confirm').addEventListener('click', () => {
     onConfirm();
-    dialogContainer.remove();
+    closeConfirmationDialog(dialogContainer);
   });
   document.body.appendChild(dialogContainer);
   dialogContainer.querySelector('dialog').showModal();
   decorateIcons(dialogContainer);
 
   dialogContainer.querySelector('.action-close').addEventListener('click', () => {
-    dialogContainer.remove();
+    if (onCancel) onCancel();
+    closeConfirmationDialog(dialogContainer);
   });
+}
+
+function closeConfirmationDialog(dialog) {
+  // remove dialog from DOM
+  dialog.remove();
+}
+
+function addContent(containerElem, content) {
+  if (typeof content === 'string') {
+    containerElem.innerText = content;
+  } else if (content instanceof HTMLElement) {
+    containerElem.appendChild(content);
+  }
 }
