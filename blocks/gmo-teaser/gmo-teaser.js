562c96d53226a12935edba45e6edbd9bf8aabf93
Michael Dickson <mdickson@adobe.com>
2023-11-14 10:22:31 -0500

    add basic footer block

--- blocks/gmo-teaser/gmo-teaser.js
+++ blocks/gmo-teaser/gmo-teaser.js
@@ -1,8 +1,4 @@
-import { readBlockConfig } from '../../scripts/lib-franklin.js';
-
 export default function decorate(block) {
-    const config = readBlockConfig(block);
-
     Array.from(block.children).forEach((child) => {
         child.classList.add("teaser");
         child.children[0]?.classList.add("title");
