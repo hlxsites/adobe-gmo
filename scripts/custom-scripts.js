e4811613ee2acf22d822f9bc0a42b68cd50cee58
Michael Dickson <mdickson@adobe.com>
2023-12-04 15:25:08 -0500

    refactor notification code
    - new branch
    - small change to immutable(?) head.html

---
+++ scripts/custom-scripts.js
@@ -0,0 +1,22 @@
+import { getBrandingConfig } from "./site-config.js";
+import { EventNames } from "./events.js";
+import showToast from "./toast-message.js";
+
+initLegalNotice();
+
+async function initLegalNotice() {
+    const config = await getBrandingConfig();
+    console.log(config);
+    document.addEventListener(EventNames.SESSION_STARTED, function() {
+      showLegalNotice(config);
+    });
+}
+  
+function showLegalNotice(config) {
+    const legalDefault = `NOTICE: Adobe records and uses your e-mail address for tracking purposes.`;
+    const legalToast = config?.legalMessage || legalDefault;
+    const legalDuration = 10000;
+    const toastType = "info";
+
+    showToast(legalToast, legalDuration, toastType, false);
+}
