24c9b8d48988cbd0bfa724a9e40a0c192235d451
Shivani gupta <shivanigupta@shivanis-mbp-2.mynetworksettings.com>
2023-12-19 12:36:53 -0500

    scripts to add experiments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -360,12 +360,21 @@ async function loadLazy(doc) {
   if (hash && element) element.scrollIntoView();
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  window.hlx.plugins.run('loadLazy');
+  
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
 
   showNextPageToast();
+
+  // Add below snippet at the end of the lazy phase
+  if ((getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length)) {
+    // eslint-disable-next-line import/no-relative-packages
+    const { loadLazy: runLazy } = await import('../plugins/experimentation/src/index.js');
+    await runLazy(document, { audiences: AUDIENCES }, pluginContext);
+  }
 }
 
 /**
