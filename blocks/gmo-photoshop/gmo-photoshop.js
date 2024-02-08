85e0d9e09d2bd891b4b25decdaaef4256137417e
Michael Dickson <mdickson@adobe.com>
2024-02-08 15:45:45 -0500

    start adding psapi functions

--- blocks/gmo-photoshop/gmo-photoshop.js
+++ blocks/gmo-photoshop/gmo-photoshop.js
@@ -1,10 +1,20 @@
 import { getAdminConfig } from '../../scripts/site-config.js';
+import { getDownloadUrl, getAssetMetadata } from '../../scripts/polaris.js';
+import { getAssetName } from '../../scripts/metadata.js';
 
 /* photoshop */
 const adminConfig = await getAdminConfig();
 const psKey = adminConfig.psClientId;
 const psSecret = adminConfig.psCS;
+const s3key = adminConfig.s3key;
+const s3secret = adminConfig.s3secret;
 let psToken;
+let s3;
+let assetJSON, assetName;
+let downloadUrl, uploadUrl;
+
+//testing purposes
+const assetId = 'urn:aaid:aem:84da1fe2-3e89-4cd0-a9e5-4171af8ea6ed'; //should I remove the urn:aaid:aem: ?
 
 // to do:
 /*
@@ -20,7 +30,14 @@ let psToken;
 
 
 export default async function decorate(block) {
-    block.innerHTML=`<dialog autofocus aria-label="Photoshop" class="ps-workshop">
+    block.innerHTML=`hello
+    <div class="button remove-bg">
+        <span class="button-text">Test Remove Background</span>
+    </div>
+    <div class="button check-status">
+        <span class="button-text">Check Status</span>
+    </div>
+    <dialog autofocus aria-label="Photoshop" class="ps-workshop">
         <div class="dialog-header">
         </div>
         <div class="dialog-body">
@@ -29,8 +46,84 @@ export default async function decorate(block) {
 
 
     // do more stuff here
+    await initAWS();
+
+    const rbgButton = block.querySelector(".remove-bg");
+    rbgButton.addEventListener('click', async () => {
+        removeBg();
+    })
+    const statusButton = block.querySelector(".check-status");
+    statusButton.addEventListener('click', async () => {
+        pollJobStatus();
+    })
 }
 
+async function initAWS() {
+    const s3region = '';
+    AWS.config.update({
+        accessKeyId: s3key,
+        secretAccessKey: s3secret,
+        region: s3region
+    });
+    s3 = new AWS.S3();
+}
+
+async function getAssetInfo() {
+    assetJSON = await getAssetMetadata(assetId);
+    assetName = getAssetName(assetJSON);
+    downloadUrl = await getDownloadUrl(assetId, assetName);
+    uploadUrl = await getUploadUrl();
+}
+
+async function removeBg() {
+    await getAssetInfo();
+    const token = getPSToken();
+    const apiUrl = 'https://image.adobe.io/sensei/cutout';
+    const storageType = 'external';
+    console.log(uploadUrl);
+    const inputs = {
+        'input': {
+            'storage': storageType,
+            'href': downloadUrl // must be public
+        },
+        'output': {
+            'storage': storageType,
+            'href': uploadUrl
+        }
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+          'X-Api-Key': psKey,
+          Authorization: token,
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'https://localhost.corp.adobe.com'
+        },
+        body: JSON.stringify(inputs),
+    };
+
+    const responseJson = await fetch(apiUrl, options).then(response => {
+        return response.json();
+    });
+    console.log(responseJson);
+}
+
+async function getUploadUrl() {
+    const filePath = 'test/psapi.jpg';
+    const bucketName = 'psapibucket'; // put actual name here
+    if (!s3) {
+        await initAWS();
+    }
+    var params = {
+        Bucket: bucketName,
+        Key: filePath, //path the uploaded file will live in
+        Expires: 60 // expiration time for the presigned URL in seconds
+    };
+ 
+    const presignedUrl = await s3.getSignedUrl('putObject', params);
+    //console.log(presignedUrl);
+    return presignedUrl;
+}
 
 async function getPSToken() {
     if (!psToken) {
@@ -49,7 +142,7 @@ async function getPSToken() {
                 'grant_type': 'client_credentials',
                 'client_secret': psSecret,
                 'scope': scope,
-                'redirect_uri': 'https://localhost.corp.adobe.com/drafts/mdickson/api-test'
+                'redirect_uri': 'https://localhost.corp.adobe.com/drafts/mdickson/psapi'
             })
         };
 
@@ -63,7 +156,7 @@ async function getPSToken() {
     }
 }
 
-async function psMask() {
+async function psMask(downloadURL, uploadURL) {
     const url = ' https://image.adobe.io/sensei/mask';
     const params = '';
     const storageType = '';
@@ -71,14 +164,14 @@ async function psMask() {
     const inputs = {
         'input': {
             'storage': storageType,
-            'href': 'imagehref'
+            'href': downloadURL // must be public
         },
         'options': {
             'optimize': 'performance'
         },
         'output': {
             'storage': storageType,
-            'href': 'place2putoutput'
+            'href': uploadURL
         }
     };
 
@@ -92,6 +185,36 @@ async function psMask() {
         },
         body: JSON.stringify(inputs),
     };
+
+}
+
+async function pollJobStatus(jobId) {
+    const statusUrl = 'https://image.adobe.io/sensei/status/' + jobId;
+    const token = await getPSToken();
+    /*
+    curl -X GET \
+  https://image.adobe.io/sensei/status/e3a13d81-a462-4b71-9964-28b2ef34aca7 \
+  -H "Authorization: Bearer $token"  \
+  -H "x-api-key: $apiKey" \
+  -H "Content-Type: application/json"
+  */
+
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'X-Api-Key': psKey,
+            Authorization: token,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'https://localhost.corp.adobe.com'
+        },
+        body: JSON.stringify(inputs),
+    };
+    const responseJson = await fetch(statusUrl, options).then(response => {
+        return response.json();
+    });
+    console.log(responseJson);
+    //const jobStatus = responseJson['access_token'];
 
 }
 
