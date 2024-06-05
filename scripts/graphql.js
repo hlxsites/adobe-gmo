2465e4382ab182fc856e6e06e8bb56bdf12e32a9
mdickson-adbe <95774602+mdickson-adbe@users.noreply.github.com>
2024-06-05 18:30:28 -0400

    fix typo in graphql query name (#111)

--- scripts/graphql.js
+++ scripts/graphql.js
@@ -103,7 +103,7 @@ export async function graphqlAllCampaignsFilter(first,cursor,filter) {
 }
 
 export async function graphqlCampaignByName(campaignName) {
-  const queryName = 'getCampaignNamesFilter';
+  const queryName = 'getCampaignNameFilter';
   const encodedCampaignName = encodeURIComponent(campaignName);
   const encodedSemiColon = encodeURIComponent(';');
   //persisted query URLs have to be encoded together with the first semicolon
