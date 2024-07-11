652faa8e4a4b2ff86f44c2fae87b8f9d6e2961f4
Tyrone Tse <ttse@adobe.com>
2024-07-11 10:19:18 -0500

    gmo-program-details.js PR Code review change to

--- scripts/assets.js
+++ scripts/assets.js
@@ -56,7 +56,7 @@ export async function searchAsset(programName, campaignName, imageWidth = 80) {
   const indexName =  await getSearchIndex();
 
 
-  if (programName==null && campaignName==null)
+  if (!programName && !campaignName)
   {
     // Display Underdevelopment Icon
     return await getUnderdevelopmentIcon();
