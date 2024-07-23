dab40f97c71540bc9f4c6988491995ffc3709759
Michael Dickson <mdickson@adobe.com>
2024-07-23 08:18:17 -0400

    year view functionality restored with refactor

--- scripts/program-calendar.js
+++ scripts/program-calendar.js
@@ -520,3 +520,334 @@ function lookupType(rawText) {
     const typeText =  typeMatch.length > 0 ? typeMatch[0].text : rawText;
     return typeText;
 }
+
+// refactor to build calendar in a new way
+// 1. determine viewable time period that tasks span
+// 2. for each year, create a calendar background
+// 3. start adding tasks
+// 4. for quarter view, 
+export async function newBuildCalendar(dataObj, block, period, type, mappingArray) {
+    if (!deliverables) deliverables = dataObj;
+    //if (!deliverables) deliverables = dataObj.data.deliverableList.items;
+    if (!deliverableMapping) deliverableMapping = await mappingArray;
+    const displayYear = period.year;
+    const displayQuarter = period.quarter;
+    const columnWidth = 8.315;
+
+
+    // get start of the view
+    const viewStart = getTimeBounds(deliverables, "start", startDateProp);
+    const viewStartYear = viewStart.getUTCFullYear();
+    // get end of the view
+    const viewEnd = getTimeBounds(deliverables, "end", endDateProp);
+    const viewEndYear = viewEnd.getUTCFullYear();
+
+    // get array of all years to be included
+    const years = calendarYears(viewStartYear, viewEndYear);
+    const numberOfYears = years.length;
+    console.log(`Years to be included: ${years}`);
+
+    // build the calendar background here as we already know the period and style
+    const calendarEl = (type === "year") ? newBuildYearCal(years) : buildQCal(displayYear);
+    // get unique deliverable types
+    const uniqueGroups = getUniqueItems(deliverables, "deliverableType");
+
+    console.log(`Start: ${viewStart} || End: ${viewEnd}`);
+
+    // set up the content wrapper
+    const contentWrapper = document.createElement('div');
+    contentWrapper.classList.add('calendar-content-wrapper');
+    if (type === "quarter") { 
+        contentWrapper.classList.add('quarter-view');
+        contentWrapper.dataset.view = "quarter";
+    } else {
+        contentWrapper.dataset.view = "year";
+    }
+
+    // adjust the column width based on # of years in view
+    //const adjustedColumnWidth = (columnWidth / numberOfYears);
+    //const adjustedColumnWidth = columnWidth;
+
+    var groupIndex = 1;
+    uniqueGroups.forEach((group) => {
+        // find all members of this group
+        const matchedItems = deliverables.filter(item => item.deliverableType === group);
+
+        // find the earliest date- this is how we set the position for the group against the calendar
+        const earliestStartDate = getTimeBounds(matchedItems, "start", startDateProp);
+        const latestEndDate = getTimeBounds(matchedItems, "end", endDateProp);
+
+        console.log(`ITEMS in GROUP ${group} -> Earliest start date: ${earliestStartDate} || Latest end date: ${latestEndDate}`)
+
+        const startMonth = (earliestStartDate.getUTCMonth()); // getMonth returns 0-11 but this is desirable
+        const startDay = (earliestStartDate.getUTCDate() - 1); // if at start of month, we don't want to add any more margin
+        const endMonth = (latestEndDate.getUTCMonth());
+        const endDay = (latestEndDate.getUTCDate() - 1);
+
+        const groupStartYear = earliestStartDate.getUTCFullYear();
+        const groupEndYear = latestEndDate.getUTCFullYear();
+        console.log(`Calendar start year: ${viewStartYear} || Group start year: ${groupStartYear} || Difference: ${groupStartYear - viewStartYear}`)
+        // accounting for different years in view
+        const startYearOffset = ((groupStartYear - viewStartYear) * 12);
+        const endYearOffset = ((groupEndYear - viewStartYear) * 12);
+        console.log(`Calendar end year: ${viewEndYear} || Group end year: ${groupEndYear} || Difference: ${viewEndYear - groupEndYear}`);
+        console.log(`Diff between group and view end years: ${viewEndYear - groupEndYear}`)
+        const totalDaysInMonth = new Date(Date.UTC(groupStartYear, startMonth, 0)).getUTCDate();
+        const totalDaysInEndMonth = new Date(Date.UTC(groupEndYear, endMonth, 0)).getUTCDate();
+
+        const percentOfStartMonth = (startDay / totalDaysInMonth);
+        const percentOfEndMonth = (endDay / totalDaysInEndMonth);
+        /* 
+        *  const dayMargin = (percentOfStartMonth * columnWidth);
+        *  const endDayMargin = (percentOfEndMonth * columnWidth);
+        *  let startPosition = (((startYearOffset + startMonth) * columnWidth) + dayMargin).toFixed(2);
+        *  let endPosition = (((endYearOffset + endMonth) * columnWidth) + endDayMargin).toFixed(2);
+        */
+        const dayMargin = (percentOfStartMonth * columnWidth);
+        const endDayMargin = (percentOfEndMonth * columnWidth);
+        let startPosition = (((startYearOffset + startMonth) * columnWidth) + dayMargin).toFixed(2);
+        let endPosition = (((endYearOffset + endMonth) * columnWidth) + endDayMargin).toFixed(2);
+        console.log(`Calculated start position (%): ${startPosition} || Calculated end position: ${endPosition}`);
+        
+
+        if (type === "quarter") {
+            // offset by a month in quarter view
+            /*
+            * startPosition = parseFloat(startPosition) + parseFloat(columnWidth);
+            * endPosition = parseFloat(endPosition) + parseFloat(columnWidth);
+            */
+            startPosition = parseFloat(startPosition) + parseFloat(columnWidth);
+            endPosition = parseFloat(endPosition) + parseFloat(columnWidth);
+        }
+            
+        // do a little offset.
+        if (endMonth > 9) endPosition = endPosition - 0.35;
+        const widthOfGroup = (endPosition - startPosition); // width of group = start position + (day duration)
+        console.log(`Start position %: ${startPosition} || End position %: ${endPosition} `)
+        // calculate the duration of the group as that helps set the width of its members
+        const groupDuration = Math.floor((latestEndDate.getTime() - earliestStartDate.getTime()) / (1000 * 60 * 60 * 24));
+
+        const itemWrapper = document.createElement('div');
+        itemWrapper.classList.add('group-content');
+
+        matchedItems.forEach((item) => {
+            const itemStartDate = new Date(item[startDateProp]);
+            const itemEndDate = new Date(item[endDateProp]);
+            console.log(`Item start date: ${itemStartDate} || Item end date: ${itemEndDate}`);
+            const itemDuration = Math.floor((itemEndDate.getTime() - itemStartDate.getTime()) / (1000 * 60 * 60 * 24));
+            const itemDurationPct = ((itemDuration / groupDuration) * 100).toFixed(2);
+
+            // Calculate the difference in months and days between earliestStartDate (group) and itemStartDate
+            //let monthsDifference = (itemStartDate.getUTCFullYear() - earliestStartDate.getUTCFullYear()) * 12 + (itemStartDate.getUTCMonth() - earliestStartDate.getUTCMonth());
+
+            //let startDaysDifference = itemStartDate.getUTCDate() - earliestStartDate.getUTCDate();
+            let daysDifference = Math.floor((itemStartDate.getTime() - earliestStartDate.getTime()) / (1000 * 60 * 60 * 24));
+            const startPctDiff = ((daysDifference / groupDuration) * 100).toFixed(2);
+
+            const itemEl = document.createElement('div');
+            itemEl.classList.add('item');
+            itemEl.style.marginLeft = startPctDiff + '%';
+
+            itemEl.innerHTML = `
+                <div class="color-tab"></div>
+                <div class="item-content">
+                    <div class="content-row">
+                        <div class="info">
+                            <div class="thumbnail"></div>
+                            <div class="name" title="${item.deliverableName}">${item.deliverableName}</div>
+                            <div class="item-status" data-status="${checkBlankString(item.taskStatus)}"></div>
+                        </div>
+                    </div>
+                    <div class="content-row bottom">
+                        ${itemStartDate ? '<div class="start-date" title="Task Start Date">' + itemStartDate.toLocaleDateString().split(',')[0] + '</div>' : ''}
+                        <div class="link">
+                            <a href="${item.reviewLink}">QA Files</a>
+                        </div>
+                    </div> 
+                </div>
+            `;
+            
+            itemEl.style.width = itemDurationPct + '%';
+            itemWrapper.appendChild(itemEl);
+
+        });
+        
+        const groupEl = document.createElement('div');
+        groupEl.classList.add('calendar-group', `color${groupIndex}`);
+        groupEl.style.marginLeft = startPosition + '%';
+        groupEl.style.width = widthOfGroup + '%';
+        groupEl.innerHTML = `
+            <div class="group-header">
+                <div class="left-block">
+                    <img src="/icons/chevron-right.svg" class="group-expand group-controls inactive"></img>
+                    <img src="/icons/chevron-right.svg" class="group-collapse group-controls"></img>
+                    <div class="group-heading">${lookupType(group)}</div>
+                    <div class="group-count">${matchedItems.length}</div>
+                </div>
+                <div class="right-block">
+                </div>
+            </div>
+            ${itemWrapper.outerHTML}
+        `;
+
+        groupEl.querySelectorAll('.group-controls').forEach((arrow) => {
+            arrow.addEventListener('click', showHideGroup);
+        });
+
+        contentWrapper.appendChild(groupEl);
+        groupIndex +=1;
+    });
+
+    calendarEl.appendChild(contentWrapper);
+    block.querySelectorAll('.year-switch > .year-toggle').forEach((control) => {
+        control.removeEventListener('click', changePeriod);
+        control.addEventListener('click', changePeriod);
+    });
+    block.querySelector('.right-controls .today-button').addEventListener('click', () => {
+        refreshCalendar({ 'year': new Date().getFullYear(), 'quarter': 1 }, "year");
+    })
+
+
+    block.querySelector('.calendar.tab').appendChild(calendarEl);
+
+    // populate "filter" dropdown
+    const filterDropdown = document.createElement('div');
+    filterDropdown.classList.add('filter-dropdown-content');
+    const uniqueYears = getUniqueYears(deliverables);
+    const yearOptionLabel = document.createElement('div');
+    yearOptionLabel.classList.add('filter-label');
+    yearOptionLabel.textContent = 'Year';
+    const quarterOptionLabel = document.createElement('div');
+    quarterOptionLabel.classList.add('filter-label');
+    quarterOptionLabel.textContent = 'Quarter';
+    filterDropdown.appendChild(yearOptionLabel);
+
+    // when choosing 'Quarter' the top left controls change to control the quarter in focus
+    // its kind of a zoomed in view.
+    uniqueYears.forEach((year) => {
+        const yearOption = document.createElement('div');
+        yearOption.classList.add('filter-option');
+        yearOption.dataset.year = year;
+        yearOption.textContent = year;
+        yearOption.addEventListener('click', filterDropdownSelection);
+        filterDropdown.appendChild(yearOption);
+    });
+    filterDropdown.appendChild(quarterOptionLabel);
+    const quarters = [ 1, 2, 3, 4 ];
+    quarters.forEach((quarter) => {
+        const quarterOption = document.createElement('div');
+        quarterOption.classList.add('filter-option');
+        quarterOption.dataset.period = quarter;
+        quarterOption.textContent = quarter;
+        quarterOption.addEventListener('click', filterDropdownSelection);
+        filterDropdown.appendChild(quarterOption);
+    })
+    
+    const filterDropdownWrapper = block.querySelector('.filter-dropdown-wrapper');
+    filterDropdownWrapper.appendChild(filterDropdown);
+    filterDropdownWrapper.querySelector('.filter-dropdown-button').addEventListener('click', (event) => toggleDropdown(event.target));
+    
+    // scroll to the right in quarter view
+    // this will work once we stop the overflow
+    if (type === "quarter") {
+        const calendarWrapper = document.querySelector('.calendar-wrapper.quarter-view')
+        const scrollPct = ((displayQuarter - 1) * (3 * columnWidth)).toFixed(2);
+        const maxScrollLeft = calendarWrapper.scrollWidth;
+        const scrollAmt = (maxScrollLeft) * (scrollPct / 100);
+        calendarWrapper.scrollLeft = scrollAmt;
+    }
+
+    // show the current month if current year chosen
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    if (displayYear == currentYear) {
+        const currentMonth = currentDate.getMonth() + 1;
+        
+        // calculate the percentage completion of the current month for the indicator offset
+        const totalDaysInMonth = new Date((new Date(currentYear, currentMonth, 1)) - 1).getDate();
+        const percentOfMonth = (currentDate.getUTCDate() / totalDaysInMonth).toFixed(2) * 100;
+
+        const monthEl = block.querySelector(`.month[data-num='${currentMonth}']`);
+        monthEl.classList.add('current');
+        const lineEl = document.createElement('div');
+        lineEl.classList.add('calendar-indicator');
+
+        // use direct style for offset
+        lineEl.style.marginRight = ((-2 * percentOfMonth) + 100) + '%';
+        monthEl.appendChild(lineEl);
+    }
+    
+    // close dropdown listener for clicks outside open dropdown
+    document.querySelector('.gmo-program-details.block').addEventListener('click', dismissDropdown);
+}
+
+function getTimeBounds(items, whichEnd, property) {
+    const desiredDate = items.reduce((dateCompare, currentItem) => {
+        const currentItemDate = new Date(currentItem[property]); // Ensure UTC
+        if (whichEnd === "start") {
+            return currentItemDate < dateCompare ? currentItemDate : dateCompare;
+        } else {
+            return currentItemDate > dateCompare ? currentItemDate : dateCompare;
+        }
+    },  new Date(items[0][property]));
+    return desiredDate;
+}
+
+function calendarYears(startYear, endYear) {
+    let years = [];
+    for (let year = startYear; year <= endYear; year++) {
+        years.push(year);
+    }
+    return years;
+}
+
+function newBuildYearCal(years) {
+    const calendarEl = document.createElement('div');
+    calendarEl.classList.add('calendar-wrapper');
+    if (years.length > 1) calendarEl.classList.add('multiyear');
+    const backgroundEl = document.createElement('div');
+    backgroundEl.classList.add('calendar-background');
+    backgroundEl.style.width = (years.length * 100) + '%';
+
+    years.forEach((year) => {
+        const yearWrapper = document.createElement('div');
+        yearWrapper.dataset.year = year;
+        yearWrapper.classList.add('year-wrapper');
+        yearWrapper.style.width = (100 / years.length) + '%';
+        const calendarHeader = document.createElement('div');
+        calendarHeader.classList.add('header-wrapper');
+        const quartersHeader = document.createElement('div');
+        quartersHeader.classList.add('quarter-header');
+        quartersHeader.innerHTML = `
+            <div class="quarter">Q1 ${year}</div>
+            <div class="quarter">Q2 ${year}</div>
+            <div class="quarter">Q3 ${year}</div>
+            <div class="quarter">Q4 ${year}</div>
+        `;
+        calendarHeader.appendChild(quartersHeader);
+
+        const monthsWrapper = document.createElement('div');
+        monthsWrapper.classList.add('month-wrapper');
+        monthsWrapper.dataset.year = year;
+        monthsWrapper.innerHTML = `
+            <div class="month" data-num="1"><div class="label">Jan</div></div>
+            <div class="month" data-num="2"><div class="label">Feb</div></div>
+            <div class="month" data-num="3"><div class="label">Mar</div></div>
+            <div class="month" data-num="4"><div class="label">Apr</div></div>
+            <div class="month" data-num="5"><div class="label">May</div></div>
+            <div class="month" data-num="6"><div class="label">Jun</div></div>
+            <div class="month" data-num="7"><div class="label">Jul</div></div>
+            <div class="month" data-num="8"><div class="label">Aug</div></div>
+            <div class="month" data-num="9"><div class="label">Sep</div></div>
+            <div class="month" data-num="10"><div class="label">Oct</div></div>
+            <div class="month" data-num="11"><div class="label">Nov</div></div>
+            <div class="month" data-num="12"><div class="label">Dec</div></div>
+        `;
+        yearWrapper.appendChild(calendarHeader);
+        yearWrapper.appendChild(monthsWrapper);
+        backgroundEl.appendChild(yearWrapper);
+    });
+    calendarEl.appendChild(backgroundEl);
+    return calendarEl;
+}
