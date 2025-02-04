e9eea74bec6a5db90f9dc9c5e95b11b886be3302
Michael Dickson <mdickson@adobe.com>
2025-02-04 12:31:43 -0500

    Add ability to hide calendar tab via block config

--- blocks/gmo-program-details/gmo-program-details.js
+++ blocks/gmo-program-details/gmo-program-details.js
@@ -24,6 +24,7 @@ const thumbnailCache = {};
 
 export default async function decorate(block) {
     blockConfig = readBlockConfig(block);
+    console.log(blockConfig)
     block.innerHTML = ` `;
 
     const currentYear = new Date().getFullYear();
@@ -66,12 +67,13 @@ export default async function decorate(block) {
     );
 
     // tab wrapper
+    const hideCalendar = blockConfig.hidecalendar;
     const tabWrapper = div(
         { class: 'tab-wrapper'},
         div({ id: 'tab1toggle', class: 'tabBtn active', 'data-target': 'tab1'}, 'Overview'),
         div({ id: 'tab2toggle', class: 'tabBtn', 'data-target': 'tab2'}, 'Deliverables'),
-        div({ id: 'tab3toggle', class: 'tabBtn', 'data-target': 'tab3'}, 'Calendar'),
-        );
+        div({ id: 'tab3toggle', class: `tabBtn ${hideCalendar ? 'inactive' : ''}`, 'data-target': 'tab3'}, 'Calendar'),
+    );
 
     // overview tab
     const overviewTab = div(
