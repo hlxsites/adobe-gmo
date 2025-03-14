b97b724592dd18fd1b1e1f187558a785e237db58
Michael Dickson <mdickson@adobe.com>
2025-03-14 13:33:08 -0400

    add changes from pr#192

--- blocks/gmo-program-list/gmo-program-list.js
+++ blocks/gmo-program-list/gmo-program-list.js
@@ -1,9 +1,14 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 import { graphqlAllCampaignsFilter, graphqlCampaignCount, generateFilterJSON } from '../../scripts/graphql.js';
-import { getProductMapping, checkBlankString, statusMapping, dateFormat, showLoadingOverlay, hideLoadingOverlay } from '../../scripts/shared-program.js'
 import { getBaseConfigPath } from '../../scripts/site-config.js';
 import { searchAsset } from '../../scripts/assets.js';
+import { toggleOption } from '../gmo-program-header/gmo-program-header.js';
+import { 
+    getProductMapping, checkBlankString, statusMapping,
+    dateFormat, showLoadingOverlay, hideLoadingOverlay,
+    div, span
+} from '../../scripts/shared-program.js'
 
 const headerConfig = [
     {
@@ -39,7 +44,6 @@ const headerConfig = [
 ]
 
 const DEFAULT_ITEMS_PER_PAGE = 8;
-//const programStatusMapping = statusMapping;
 //Global variables used by helper functions
 let currentPageInfo = {};
 let cursorArray = [];
@@ -70,6 +74,10 @@ document.addEventListener('gmoCampaignListBlock', async function() {
     cursorArray = [];
     currentPage = 1;
     currentNumberPerPage = DEFAULT_ITEMS_PER_PAGE;
+
+    // save the filter in a cookie
+    createSearchFilterCookie(currentGraphqlFilter);
+
     //Trigger loading the gmo-campaign-block
     decorate( block, currentNumberPerPage, '', false, false, currentGraphqlFilter);
 });
@@ -77,6 +85,20 @@ document.addEventListener('gmoCampaignListBlock', async function() {
 export default async function decorate(block, numPerPage = currentNumberPerPage, cursor = '', previousPage = false, nextPage = false, graphQLFilter = {}) {
     showLoadingOverlay(block);
     if (blockConfig == undefined) blockConfig = readBlockConfig(block);
+
+    // check if this was a 'back' from details. if so, retrieve search params from cookie
+    const params = new URLSearchParams(window.location.search);
+    const isBack = params.get('isBack');
+    // clear the params from the url
+    clearURLParams();
+    // retrieve previous search from cookie
+    if (isBack) { 
+        const filterValue = getFilterFromCookie();
+        if (filterValue) graphQLFilter = JSON.parse(filterValue);
+        // add filter values to filter list
+        displayFilterSelections(graphQLFilter);
+    }
+
     const campaignPaginatedResponse = await graphqlAllCampaignsFilter(numPerPage, cursor,graphQLFilter);
     const campaigns = campaignPaginatedResponse.data.programPaginated.edges;
     currentPageInfo = campaignPaginatedResponse.data.programPaginated.pageInfo;
@@ -550,4 +572,33 @@ function sortColumn(dir, property) {
     sortArray.forEach(({ row }, index) => {
         container.appendChild(row);
     });
+}
+
+function createSearchFilterCookie(graphQLFilter) {
+    const date = new Date();
+    date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
+    const expires = `expires=${date.toUTCString()}`;
+    const searchParams = JSON.stringify(graphQLFilter);
+    document.cookie = `MH_PROGRAM_FILTER=${encodeURIComponent(searchParams)}; ${expires}; path=/`;
+}
+
+function getFilterFromCookie() {
+    const cookieName = 'MH_PROGRAM_FILTER';
+    const cookie = document.cookie.match(new RegExp(`(?:^|; )${cookieName}=([^;]*)`));
+    return cookie ? decodeURIComponent(cookie[1]) : null; // Return null if the cookie is not found 
+}
+
+function clearURLParams() {
+    const currentUrl = window.location.href;
+    const baseUrl = currentUrl.split('?')[0];
+    history.replaceState(null, '', baseUrl);
+}
+
+function displayFilterSelections(filterObj) {
+    for (const key in filterObj) {
+        if (filterObj[key]._expressions && Array.isArray(filterObj[key]._expressions)) {
+            const value = filterObj[key]._expressions[0].value;
+            toggleOption(value, key);
+        }
+    }
 }
